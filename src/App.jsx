55e72ca927126f30fefe32bdb51299712d004329
solovyovsdom <joelborofsky@mac.com>
2022-11-25 11:53:59 -0600

    updated theme color

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
+import { useState } from 'react'
 import './scss/index.scss'
 
 function App() {
-  
+  const [theme, setTheme] = useState('light');
+
+  function toggleTheme(){
+    const newTheme = theme === 'dark' ? 'light' : 'dark'
+    setTheme(newTheme)
+  }
 
   return (
-    <div >
-      <h1>What the fuck?</h1>
+    <div data-theme={theme}>
+      <h1>Hello there</h1>
     </div>
   )
 }
