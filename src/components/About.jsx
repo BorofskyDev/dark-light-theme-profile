0b460b5711716c4b89eaf0a98cdc869cf329b09f
solovyovsdom <joelborofsky@mac.com>
2022-11-26 00:35:18 -0600

    created about page, need to make responsive

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,50 @@
+import { FaGithub, FaLinkedinIn, FaCodepen, FaTwitter, FaLink, FaCode } from 'react-icons/fa'
+import joel2 from '../assets/personal-images/me-kyiv.jpg'
 
 function About() {
   return (
-    <div></div>
+    <section className='about'>
+      <h2 className='section__title section__title--about'>Who I am</h2>
+      <p className='section__subtitle section__subtitle--about'>
+        Developer and Designer based in the USA
+      </p>
+      <div className='About__body'>
+        <p>
+          Currently based out of Wichita, KS, I am a frontend developer with an
+          eye for design. I am unabashedly in love with CSS, ThreeJS, and React
+          Three Fiber (especially Drei!). But I also love playing around with
+          data and seeing how that data can translate to the frontend, and have
+          a firm belief that any design project must begin and end with
+          understanding the backend.
+        </p>
+        <br />
+        <p>
+          In my downtime you'll find me playing Cities: Skylines (a lot),
+          working on my own projects, taking walks, working out, and when time
+          and money permit, traveling. I enjoy experiencing different cultures
+          and have a host of places that I want to visit.
+        </p>
+      </div>
+      <img
+        src={joel2}
+        alt='Joel standing with arms crossed and a bird on his shoulder, in Maidan Square in Kyiv, Ukraine'
+        className='About__img'
+      />
+      <div className='About__social-media'>
+        <a href='#' className='About__social-media--link'>
+          <FaGithub className='About__social-media--icon github' />
+        </a>
+        <a href='#' className='About__social-media--link'>
+          <FaLinkedinIn className='About__social-media--icon linkedin' />
+        </a>
+        <a href='#' className='About__social-media--link'>
+          <FaCodepen className='About__social-media--icon codepen' />
+        </a>
+        <a href='#' className='About__social-media--link'>
+          <FaTwitter className='About__social-media--icon twitter' />
+        </a>
+      </div>
+    </section>
   )
 }
 
