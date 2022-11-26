5703c7b70414f11660120382c2c845c038ca753b
solovyovsdom <joelborofsky@mac.com>
2022-11-25 21:43:56 -0600

    finished header and sidenav

--- src/components/util/Header.jsx
+++ src/components/util/Header.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
+function Header(props) {
+  const { menu, setMenu } = props
 
-function Header() {
   return (
-    <div>Header</div>
+    <header className='Header'>
+      <div className='Header__logo'>
+        <p className='Header__logo--title stronger'>
+          &#123; <span className='Header__logo--borofsky'>B</span>orofsky
+          <span className='Header__logo--dev'>D</span>ev &#125;
+        </p>
+      </div>
+      <div className='Header__button'>
+        <button
+          onClick={() => setMenu(!menu)}
+          className={`menu__trigger ? ${menu ? 'menu__close' : ''}`}
+        >
+          <span></span>
+          <span></span>
+          <span></span>
+          
+          <p className='sr-only'>Menu Toggle</p>
+        </button>
+      </div>
+    </header>
   )
 }
 
--- src/components/util/Sidebar.jsx
+++ src/components/util/Sidebar.jsx
@@ -1,8 +1,60 @@
-import React from 'react'
+import { useEffect, useRef } from 'react'
+import gsap from 'gsap'
 
-function Sidebar() {
+function Sidebar(props) {
+  const { menu, setMenu } = props
+  let sidebarMenu = useRef(null)
+  let sidebarMenuOverlay = useRef(null)
+  let menuLayer = useRef(null)
+  let menuTimeline = useRef(null)
+
+  useEffect(() => {
+    menuTimeline.current = gsap.timeline({ paused: true })
+    menuTimeline.current.fromTo(
+      [sidebarMenuOverlay, menuLayer, sidebarMenu],
+      { duration: 0, x: '100%' },
+      {
+        duration: 0.75,
+        x: '0%',
+        ease: 'power3.inOut',
+        stagger: {
+          amount: 0.5,
+        },
+      }
+    )
+  }, [])
+
+  useEffect(() => {
+    menu ? menuTimeline.current.play() : menuTimeline.current.reverse()
+  }, [menu])
+
   return (
-    <div>Sidebar</div>
+    <div className='Menu' ref={(el) => (sidebarMenuOverlay = el)}>
+      <div className='Menu__wrapper'>
+        <div className='Menu__layer' ref={(el) => (menuLayer = el)}></div>
+        <nav className="Menu__nav" ref={(el) => (sidebarMenu = el)} >
+          <div className="Menu__top">
+            <ul className="Menu__links">
+              <li className="Menu__link">
+                <a href="#" className="Menu__link--section">
+                  About
+                </a>
+              </li>
+              <li className="Menu__link">
+                <a href="#" className="Menu__link--section">
+                  Projects
+                </a>
+              </li>
+              <li className="Menu__link">
+                <a href="#" className="Menu__link--section">
+                  Contact
+                </a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </div>
+    </div>
   )
 }
 
