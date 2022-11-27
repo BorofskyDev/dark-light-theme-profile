a86acde990f516e504ccbd3a1a5ac84b47547495
solovyovsdom <joelborofsky@mac.com>
2022-11-26 23:54:33 -0600

    outline of contact form done, needs styling

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,11 +1,91 @@
 function Contact() {
   return (
     <section className='Contact'>
-      <div className="Contact__content-container">
+      <div className='Contact__content-container'>
         {/* top/left content */}
+        <div className='Contact__header'>
+          <h2 className='Contact__title'>Contact</h2>
+          <h2 className='Contact__title--bottom'>Me</h2>
+        </div>
+        <div className='Contact__links'>
+          <h3 className='Contact__links--title'>Also find me here:</h3>
+          <a href='#' className='Contact__links--link'>
+            GitHub
+          </a>
+          <a href='#' className='Contact__links--link'>
+            LinkedIn
+          </a>
+          <a href='#' className='Contact__links--link'>
+            Twitter
+          </a>
+          <a href='#' className='Contact__links--link'>
+            CodePen
+          </a>
+        </div>
+        <p className='Contact__email'>
+          Form not working? joelborofskydev@gmail
+        </p>
       </div>
-      <div className="Contact__form">
+      <div className='Contact__form-container'>
         {/* Form */}
+        <form
+          action='https://formsubmit.co/joelborofskydev@gmail.com'
+          method='post'
+          className='Contact__form'
+        >
+          <div className='Contact__form-control'>
+            <label htmlFor='full name' className='sr-only'>
+              Enter your name
+            </label>
+            <input
+              type='text'
+              name='full name'
+              min='1'
+              max='30'
+              placeholder='Name'
+              required
+              className='Contact__form-control--input'
+            />
+          </div>
+          <div className='Contact__form-control'>
+            <label htmlFor='email' className='sr-only'>
+              Enter your email
+            </label>
+            <input
+              type='email'
+              name='email address'
+              placeholder='Email'
+              required
+              className='Contact__form-control--input'
+            />
+          </div>
+          <div className='Contact__form-control'>
+            <label htmlFor='subject' className='sr-only'>
+              Enter the subject of the email
+            </label>
+            <input
+              type='text'
+              name='service'
+              min='3'
+              max='40'
+              placeholder='Subject'
+              required
+              className='Contact__form-control--input'
+            />
+          </div>
+          <div className='Contact__form-control'>
+            <label htmlFor='message' className='sr-only'>
+              Enter your message
+            </label>
+            <textarea
+              name='message'
+              placeholder='Message'
+              required
+              className='Contact__form-control--input'
+            />
+          </div>
+          <button className="Contact__button">Send Message</button>
+        </form>
       </div>
     </section>
   )
